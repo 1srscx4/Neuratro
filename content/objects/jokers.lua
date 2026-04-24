54f33ff2af3377ec014d3ea1ac1d754b179fbe07
Adesi <kopietz123@gmail.com>
2026-04-24 11:25:05 +0200

    fix(PaulaMarina): planet cards should no longer crash the game when triggered

--- content/objects/jokers.lua
+++ content/objects/jokers.lua
@@ -5430,7 +5430,7 @@ SMODS.Joker({
 					delay = 0.1,
 					func = function()
 						if consumed_card and not consumed_card.removed and consumed_card.area then
-							consumed_card:use(consumed_card.config.center, consumed_card)
+							consumed_card:use_consumeable(consumed_card.config.center, consumed_card)
 						end
 						return true
 					end,
